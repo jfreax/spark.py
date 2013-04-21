4b78fc3de7d4da9c1b9d9b6525ff4121460b8a07
Jens Dieskau <jfreax@googlemail.com>
2013-04-21 15:46:41 +0200

    Copyright / License text

--- spark.py
+++ spark.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python2
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# spark.py-  https://github.com/jfreax/spark.py
+# Print sparks like ▁▂▃▅▂▇ in your shell or use it in your python projects
+#
+# Copyright (C) 2013  Jens Dieskau
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License 2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 import sys
 import select
